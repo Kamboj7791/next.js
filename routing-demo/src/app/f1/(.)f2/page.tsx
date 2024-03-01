0dbdcc53f5d453481ef647e5083d229b625adcbc
kamboj7791 <kambojkunal01@gmail.com>
2024-03-02 00:15:41 +0530

    intercepting routes

---
+++ routing-demo/src/app/f1/(.)f2/page.tsx
@@ -0,0 +1,7 @@
+export default function interceptedF2() {
+  return (
+    <>
+      <h1>(.) intercepted F2 page</h1>
+    </>
+  );
+}
---
+++ routing-demo/src/app/f1/f4/(...)about/page.tsx
@@ -0,0 +1,7 @@
+export default function InterceptedAboutpage() {
+  return (
+    <>
+      <h1>(...) intercepted about page</h1>
+    </>
+  );
+}
