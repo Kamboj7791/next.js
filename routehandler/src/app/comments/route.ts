fa68f8d3474e6597d8d49be7fcd0e9b8f8bcd1e2
kamboj7791 <kambojkunal01@gmail.com>
2024-03-08 00:54:26 +0530

    post request

--- routehandler/src/app/comments/route.ts
+++ routehandler/src/app/comments/route.ts
@@ -3,3 +3,17 @@ import { Comments } from "./data";
 export async function GET() {
   return Response.json(Comments);
 }
+export async function POST(request: Request) {
+  let comment = await request.json();
+  const newComment = {
+    id: Comments.length + 1,
+    text: comment.text,
+  };
+  Comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
+}
