18d9cc7cf993dc4fc264dbf6b831409395e2e938
kamboj7791 <kambojkunal01@gmail.com>
2024-03-09 00:30:40 +0530

    patch request

--- routehandler/src/app/comments/[id]/route.ts
+++ routehandler/src/app/comments/[id]/route.ts
@@ -1,11 +1,23 @@
 import { Comments } from "../data";
 
 export async function GET(
-  request: Request,
+  _request: Request,
   { params }: { params: { id: string } }
 ) {
   const comment = Comments.find(
     (comment) => comment.id === parseInt(params.id)
   );
   return Response.json(comment);
+}
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const { text } = body;
+  const index = Comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  Comments[index].text = text;
+  return Response.json(Comments[index]);
 }
