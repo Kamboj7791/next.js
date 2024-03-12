7c7c1a2ef394ac6adb068420de0bfb191de1c5d6
kamboj7791 <kambojkunal01@gmail.com>
2024-03-13 00:04:39 +0530

    cookies

--- routehandler/src/app/profile/api/route.ts
+++ routehandler/src/app/profile/api/route.ts
@@ -1,14 +1,20 @@
 import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 
 export async function GET(request: NextRequest) {
   const Header = new Headers(request.headers);
   const newHeader = headers();
+  cookies().set("requestPerPage", "23");
+
+  const theme = request.cookies.get("theme");
   console.log(Header.get("Authorization"));
   console.log(newHeader.get("Authorization"));
+  console.log(theme);
+  console.log(cookies().get("requestPerPage"));
   return new Response("<h1>api profile data</h1>", {
     headers: {
       "content-type": "text/html",
+      "set-Cookie": "theme = dark",
     },
   });
 }
