8f1c2a79982eab24f423784cf20c9da062dc729a
kamboj7791 <kambojkunal01@gmail.com>
2024-03-12 23:34:01 +0530

    headers

--- routehandler/src/app/profile/api/route.ts
+++ routehandler/src/app/profile/api/route.ts
@@ -1,3 +1,7 @@
-export async function GET() {
+import { type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const Header = new Headers(request.headers);
+  console.log(Header.get("Authorization"));
   return new Response("api profile data");
 }
